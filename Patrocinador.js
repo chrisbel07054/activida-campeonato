9f14f63d1b0ea0dfd89f53a45aa46854100475d1
chrisbel07054 <bricenobricenoca@uvm.edu.ve>
2023-11-19 02:01:27 -0400

    funciones actualizar y eliminar patrocinadores

--- Patrocinador.js
+++ Patrocinador.js
@@ -64,3 +64,73 @@ const postPatrocinador = async (req, resp = response ) => {
         }
 }
 
+const putPatrocinador  = async (req, resp = response ) => {
+    const uid = req.params.id
+    try {
+        const patrocinadorDb = await Patrocinador.findById(uid)
+
+        if(!patrocinadorDb){
+            return resp.status(400).json({
+                ok:false,
+                msg:'Esta Patrocinador no existe'
+            })
+            }
+
+            const patrocinios = ["Diamante - 100$" , "Oro - 80$", "Plata - 60$", "Bronce - libre", "Padrino - libre"]
+
+            if(!patrocinios.includes(patrocinadorDb.Patrocinio)){
+                 resp.status(500).json({
+                     ok:false,
+                     msg:"Ese patrocinio no existe, recuerda que los que existen son: Diamante - 100$, Oro - 80$, Plata - 60$, Bronce - libre, Padrino - libre"
+                 })
+            } 
+
+        const patrocinador = await Patrocinador.findByIdAndUpdate(uid, req.body, {new: true})
+
+        resp.json({
+            ok:true,
+            patrocinador
+            })  
+       
+    } catch (error) {
+        console.log(error)
+        resp.status(500).json({
+            ok:false,
+            msg:'Error inesperado... reivsar logs'
+        })
+    }
+}
+
+
+const deletePatrocinador = async( req, resp = response ) => {
+const uid = req.params.id
+try {
+const patrocinadorDB = await Patrocinador.findById(uid)
+
+ //Verificacion de que ya exista el Patrocinador
+if(!patrocinadorDB){
+    return resp.status(400).json({
+        ok:false,
+        msg:'Esta Patrocinador no existe'
+    })
+    }
+
+    //Eliminación
+    const patrocinador = await Patrocinador.findByIdAndDelete(uid)
+
+        resp.json({
+        ok:true,
+        msg: 'Patrocinador eliminado'
+        })  
+        
+    } catch (error) {
+        console.log(error);
+        resp.status(500).json({
+            ok:'false',
+            msg:'Error inesperado... reivsar logs'
+        })
+    }
+}
+
+
+module.exports = { getPatrocinador, postPatrocinador, putPatrocinador, deletePatrocinador}
